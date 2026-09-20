cd321dd0c62a50ec428f55b341ee75138ba4c967
daniilosg <danilosg@live.com>
2026-09-19 21:07:26 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -6,20 +6,20 @@ window.CozinhaComLuConfig = {
   // VISIBILIDADE
   // true = mostrar | false = ocultar
   mostrarBonus: false, // .bonus-bridge + .bonuses
-  mostrarPlanoCompleto: false, // .alerta-up + .plano-recomendado-wrap
+  mostrarPlanoCompleto: true, // .alerta-up + .plano-recomendado-wrap
 
   // PLANOS, PREÇOS E LINKS DE PAGAMENTO
   planos: {
     basico: {
-      precoAntes: '€49,90',
-      preco: '€9,90',
-      linkPagamento: 'https://pay.hotmart.com/C107687635H?off=ihndelia'
+      precoAntes: '€29,90',
+      preco: '€7,90',
+      linkPagamento: 'https://pay.hotmart.com/C107687635H?off=zifhv669'
     },
 
     completo: {
-      precoAntes: '€169,90',
-      preco: '€37,90',
-      linkPagamento: 'https://pay.hotmart.com/V103624520G?checkoutMode=10'
+      precoAntes: '€49,90',
+      preco: '€12,90',
+      linkPagamento: 'https://pay.hotmart.com/C107687635H?off=ihndelia'
     }
   }
 };
